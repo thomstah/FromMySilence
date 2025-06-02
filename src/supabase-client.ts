b45d2ae527bbf339b4a8f4a80e5a64b9628d98b3
thomstah <thommyxay@gmail.com>
2025-06-02 10:19:45 -0500

    ifnally fixed problem

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://nzjtinwdewlisnzcsgcn.supabase.co';
-const supabaseAnonKey = import.meta.env.SUPABASE_ANON as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
