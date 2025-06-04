134191c3d3944100da7ee9de59471614c16ad7cb
thomstah <thommyxay@gmail.com>
2025-06-04 00:32:12 -0500

    implementing different post it

--- src/pages/Send.tsx
+++ src/pages/Send.tsx
@@ -1,9 +1,8 @@
-import { CreateLetter } from "../components/LetterForm";
+import { CreateLetter } from '../components/LetterForm';
 
 export const SendLetter = () => {
   return (
     <div className="container mt-25 flex justify-center pt-10">
-      <h2> Send a Letter </h2>
       <CreateLetter />
     </div>
   );
